a9bda843705d0ef35fd23d75e522dd6f15a11573
mohammad-ziadeh <mohammedziadeh6@gmail.com>
2025-12-11 18:24:09 +0300

    adding users rpute to the domain

--- app/(dashboard)/dashboard/users/page.tsx
+++ app/(dashboard)/dashboard/users/page.tsx
@@ -2,19 +2,17 @@ import Link from "next/link";
 import type { User } from "@/app/lib/definitions";
 
 
-// async function getUsers(): Promise<User[]> {
-//   // const response = await fetch("https://nextjs-learning-one-sand.vercel.app/api/users");
-
-//   // const response = await fetch("http://localhost:3000/api/users");
-//   // if (!response.ok) throw new Error("failed to fetch data");
+async function getUsers(): Promise<User[]> {
+  const response = await fetch("http://192.168.1.113:3000/api/users");
+  if (!response.ok) throw new Error("failed to fetch data");
 
 
-//   // return response.json();
-// }
+  return response.json();
+}
 
 
 export default async function User() {
-  // const users = await getUsers();
+  const users = await getUsers();
   return (
     <div className="grid min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Hello this is users page</h1>
@@ -28,7 +26,7 @@ export default async function User() {
         <li>
           <Link href="/dashboard/users/3">User 3</Link>
         </li> */}
-        {/* {users.map(({ id, name }) => (
+        {users.map(({ id, name }) => (
           <li key={id}>
             <Link
               href={`/dashboard/users/${id}`}
@@ -38,7 +36,7 @@ export default async function User() {
             </Link>
           </li>
 
-        ))} */}
+        ))}
       </ul>
     </div>
   );
